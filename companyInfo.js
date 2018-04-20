8d10d600ebb5ccd6d9d84e2567d3bab31a33d686
Julia Nai <nai.julia@gmail.com>
2018-04-19 22:38:17 -0700

    added some z

--- companyInfo.js
+++ companyInfo.js
@@ -1,39 +1,38 @@
+employee_countz = ' '
+employee_linkz = ' '
+company_websitez = ' '
+company_discriptionz = ' '
+
 let a = document.getElementsByTagName("a");
 
 for (elt of a) {
     if (elt.innerHTML.includes("employees on LinkedIn")) {
-        employee_count = elt.innerHTML
-        employee_link = elt.href
+        employee_countz = elt.innerHTML;
+        employee_linkz = elt.href;
         break;
     }
 }
 
-company_website = ' '
 for (elt of a) {
     if (elt.innerHTML.includes("Website")) {
-        company_website = elt.href
+        company_websitez = elt.href;
         break;
     }
 }
 
-company_discription = ' '
 for (elt of a) {
     if (elt.classList.contains("topcard-see-more-link")) {
         elt.click();
         let p = document.getElementsByTagName("p");
             for (elt_p of p) {
                 if (elt_p.classList.contains("topcard-extended-description-modal-content-text")) {
-                    company_discription = String(elt_p.innerHTML)
+                    company_discriptionz = String(elt_p.innerHTML);
                 }
             }
     }
 }
 
-console.log(employee_count)
-console.log(employee_link)
-console.log(company_website)
-console.log(company_discription)
-// alert(employee_count)
-// alert(employee_link)
-// alert(company_website)
-// alert(company_discription)
+console.log(employee_countz)
+console.log(employee_linkz)
+console.log(company_websitez)
+console.log(company_discriptionz)
