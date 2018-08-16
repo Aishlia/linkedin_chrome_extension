9175072b0b0c4d8b808b3952cbefe1fbf1357031
Eduardo Morales <eduardo@Eduardos-MacBook-Pro.local>
2018-08-16 14:12:18 -0700

    Update

--- saveLeads.js
+++ saveLeads.js
@@ -1,12 +1,13 @@
-function send_info(name, company, title) {
+function send_info(name, company, company_url, title) {
   var request = new XMLHttpRequest();
 
   name = "name=" + name;
-  company = "&company=" + company
+  company = "&company=" + company;
+  company_url = "&website=" + company_url;
   title = "&title=" + title;
 
   var url = 'http://127.0.0.1:8000/contacts/'
-  var params = name + company + title;
+  var params = name + company + company_url + title;
 
   request.open(
     "POST", url, true
@@ -38,8 +39,9 @@ for (elt of results_list) {
     try {
       name = elt.getElementsByClassName('name')[0].firstChild.innerHTML
       company = encodeURIComponent(elt.getElementsByClassName('company-name')[0].innerHTML)
+      company_url = encodeURIComponent(elt.getElementsByClassName('company-name')[0].href)
       position = encodeURIComponent(elt.getElementsByClassName('info-value')[0].innerHTML.replace(/<[/]b>/g, "").replace(/<b>/g, ''))
-      send_info(name, company, position)
+      send_info(name, company, company_url, position)
     } catch (err) {
       console.log("someone has missing info")
     }
